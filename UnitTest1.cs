058466f7584324454a8e076adabf5fc12e4c3fbb
Adonge2d <Adonge2d@ADONGE2D>
2021-10-20 12:54:16 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SampleMSTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
